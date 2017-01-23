1d3daf8bb724a00b18f71cfaadb21452b50b99c3
Adam Bobowski <bobowskiadam@gmail.com>
2017-01-23 14:04:50 +0100

    Split server into logic & server

--- blackjack/server.py
+++ blackjack/server.py
@@ -2,8 +2,40 @@ from flask import Flask, request, jsonify
 
 from blackjack.logic import Player, Table
 
+
+app = Flask(__name__)
 clients = {}
 tables = {}
+
+
+# Server return messages
+def error(msg):
+    return jsonify({"header": "error", "message": msg})
+
+
+def confirm_register(cid):
+    return jsonify({"header": "confirm_register", "id": cid})
+
+
+def game_state(table):
+    return None
+
+
+@app.route('/begin', methods=['POST'])
+def start_game():
+    json = request.get_json()
+
+    if json["header"] != "register":
+        return error("Invalid command header: " + json["header"])
+
+    try:
+        cid = max(clients) + 1 if clients else 0
+        clients[cid] = Player(json["cash"])
+        return confirm_register(cid)
+    except Exception as e:
+        return error(str(e))
+
+
 actions = {
     "split": Player.split,
     "double": Player.double,
@@ -14,54 +46,19 @@ actions = {
 }
 
 
-app = Flask(__name__)
+@app.route('/player-<int:cid>', methods=['POST'])
+def handle_request(cid: int):
+    json = request.get_json()
 
-
-# Handling requests
-
-
-# Game beginning
-
-def error(msg):
-    return jsonify({"header": "error", "message": msg})
-
-
-last_id = -1
-
-
-def get_id():
-    global last_id
-    last_id += 1
-    return last_id
-
-
-@app.route('/begin', methods=['POST'])
-def start_game():
-    input_json = request.get_json()
-    if input_json["header"] == "register":
-        try:
-            cid = get_id()
-            clients[cid] = Player(input_json["cash"])
-            return jsonify({"header": "confirm_register", "id": cid})
-        except Exception as e:
-            return error(str(e))
-
-    else:
-        return error("Invalid command")
-
-
-@app.route('/player-<client_id>', methods=['POST'])
-def handle_request(client_id):
-    input_json = request.get_json()
     try:
-        cid = int(client_id)
-        if input_json["header"] == "begin_game":
-            tid = input_json["table_id"]
+        cid = int(cid)
+        if json["header"] == "begin_game":
+            tid = json["table_id"]
             if tid not in tables.keys() or tables[tid] is not None:
                 tables[tid] = Table()
-            clients[cid].join_table(tables[tid], int(input_json["bid"]))
+            clients[cid].join_table(tables[tid], int(json["bid"]))
         else:
-            actions[input_json["header"]](clients[cid])
+            actions[json["header"]](clients[cid])
         if clients[cid].table is None:
             raise Exception("You are not in a game")
         return jsonify(clients[cid].to_dict())
