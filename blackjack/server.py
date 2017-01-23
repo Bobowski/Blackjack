c89cd1f956928ee8a854942f683728aa8c93230b
Adam Bobowski <bobowskiadam@gmail.com>
2017-01-23 14:51:51 +0100

    Divided request handlers into separate functions

--- blackjack/server.py
+++ blackjack/server.py
@@ -18,49 +18,35 @@ def confirm_register(cid):
 
 
 def game_state(table):
-    return None
+    return jsonify(table.to_dict())
 
 
-@app.route('/begin', methods=['POST'])
-def start_game():
-    json = request.get_json()
-
-    if json["header"] != "register":
-        return error("Invalid command header: " + json["header"])
-
-    try:
-        cid = max(clients) + 1 if clients else 0
-        clients[cid] = Player(json["cash"])
-        return confirm_register(cid)
-    except Exception as e:
-        return error(str(e))
+@app.route('/register', methods=['POST'])
+def register():
+    cid = max(clients) + 1 if clients else 0
+    clients[cid] = Player(request.json["cash"])
+    return confirm_register(cid)
 
 
-actions = {
-    "split": Player.split,
-    "double": Player.double,
-    "insure": Player.insure,
-    "pass": Player.pas,
-    "get": Player.get_card,
-    "quit": Player.quit_table
-}
+@app.route('/player/<int:cid>/begin', methods=['POST'])
+def begin_game(cid: int):
+    tid = request.json["table_id"]
+    if tid not in tables.keys() or tables[tid] is not None:
+        tables[tid] = Table()
+    clients[cid].join_table(tables[tid], int(request.json["bid"]))
+
+    return game_state(clients[cid])
 
 
-@app.route('/player-<int:cid>', methods=['POST'])
-def handle_request(cid: int):
-    json = request.get_json()
-
-    try:
-        cid = int(cid)
-        if json["header"] == "begin_game":
-            tid = json["table_id"]
-            if tid not in tables.keys() or tables[tid] is not None:
-                tables[tid] = Table()
-            clients[cid].join_table(tables[tid], int(json["bid"]))
-        else:
-            actions[json["header"]](clients[cid])
-        if clients[cid].table is None:
-            raise Exception("You are not in a game")
-        return jsonify(clients[cid].to_dict())
-    except Exception as e:
-        return error(str(e))
+@app.route('/player/<int:cid>/action', methods=['POST'])
+def make_action(cid: int):
+    actions = {
+        "split": Player.split,
+        "double": Player.double,
+        "insure": Player.insure,
+        "pass": Player.pas,
+        "get": Player.get_card,
+        "quit": Player.quit_table
+    }
+    actions[request.json["action"]](clients[cid])
+    return game_state(clients[cid])
