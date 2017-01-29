06c871863370ff26196efc302a64050beb21889e
Adam Bobowski <bobowskiadam@gmail.com>
2017-01-29 15:39:15 +0100

    I dont know

--- blackjack/game/decks.py
+++ blackjack/game/decks.py
@@ -1,5 +1,4 @@
 from random import Random
-from typing import List
 
 
 class Card:
@@ -11,24 +10,27 @@ class Card:
         self.rank = rank
         self.face_up = face_up
 
-    # def to_dict(self):
-    #     return {
-    #         "color": self.color if self.face_up else "face_down",
-    #         "rank": self.rank if self.face_up else "face_down"
-    #     }
+    def __eq__(self, other):
+        return self.color == other.color and self.rank == other.rank
 
 
 class Decks:
-    def __init__(self, seed: int=42):
+    def __init__(self, seed: int=42, count: int=1):
         self.random = Random(seed)
+        self.count = count
         self.cards = []
+        self._prepare()
+
+    def _prepare(self):
+        self.cards = [
+            Card(color=color, rank=rank)
+            for color in Card.colors
+            for rank in Card.ranks
+            for _ in range(self.count)
+        ]
+        self.random.shuffle(self.cards)
 
     def get(self) -> Card:
         if len(self.cards) == 0:
-            self.cards = [
-                Card(color=color, rank=rank)
-                for color in Card.colors
-                for rank in Card.ranks
-            ]
-            self.random.shuffle(self.cards)
+            self._prepare()
         return self.cards.pop()
--- blackjack/game/table.py
+++ blackjack/game/table.py
@@ -1,4 +1,5 @@
 from functools import wraps
+from typing import List
 
 from namedlist import namedlist
 
@@ -16,15 +17,29 @@ class InvalidMove(Exception):
 
 class Hand:
     def __init__(self):
-        self.playing = True
+        self.playing = False
         self.cards = []
 
     def add(self, card: Card, face_up: bool=True):
+        self.playing = True
         card.face_up = face_up
         self.cards.append(card)
 
     def clear(self):
         self.cards.clear()
+
+    @property
+    def value(self) -> int:
+        aces = len([x for x in self.cards if x.rank == 1])
+        value = sum([10 if x.rank > 10 else x.rank for x in self.cards]) + aces * 10
+
+        if value <= 21 or aces == 0:
+            return value
+
+        while aces > 0 and value > 21:
+            value -= 10
+            aces -= 1
+        return value
 
 
 class Player:
@@ -42,12 +57,17 @@ class Player:
             raise InvalidMove("Negative account balance is not allowed")
         self._balance = value
 
+    def switch_hand(self):
+        if self.hands[1].playing:
+            self.hands = (self.hands[1], self.hands[0])
+
     @property
-    def hand(self) -> Hand or None:
-        for hand in self.hands:
-            if hand.playing:
-                return hand
-        return None
+    def hand(self) -> Hand:
+        return self.hands[0]
+
+    @property
+    def other_hand(self):
+        return self.hands[1]
 
     def clear(self):
         for hand in self.hands:
@@ -62,17 +82,6 @@ class Croupier:
         self.hand.clear()
 
 
-def game_action(*phases):
-    def decorator(f):
-        @wraps(f)
-        def wrapper(self: Table, *args, **kw):
-            if self.state.phase in phases:
-                return f(*args, **kw)  # TODO if_end?
-            raise InvalidMove("Not in proper phase")
-        return wrapper
-    return decorator
-
-
 class Table:
     def __init__(self, account_balance: int):
         self.state = State(phase="awaiting", bid=0, winner=None)
@@ -84,7 +93,35 @@ class Table:
         self.did_double = False
         self.did_insure = False
 
-    def clear(self):
+    def game_action(*phases):
+        def decorator(f):
+            @wraps(f)
+            def wrapper(self, *args, **kw):
+                if self.state.phase not in phases:
+                    raise InvalidMove("Not in proper phase")
+
+                f(self, *args, **kw)
+
+                if self.player.hand.value > 21:
+                    self.player.hand.playing = False
+                    return
+
+                if self.player.other_hand.playing:
+                    self.player.switch_hand()
+                    return
+
+                if not self.player.hand.playing:
+                    self.state.phase = "end_game"
+
+                    self.croupier.hand.cards[0].face_up = True
+                    self.croupier.hand.cards[1].face_up = True
+                    while self.croupier.hand.value <= 16:
+                        self.croupier.hand.add(self.decks.get())
+
+            return wrapper
+        return decorator
+
+    def clear(self):  # TODO
         self.state = State(phase="awaiting", bid=0, winner=None)
         self.croupier.clear()
         self.player.clear()
@@ -93,28 +130,38 @@ class Table:
     def begin_game(self, bid: int):
         self.player.account_balance -= bid
         self.state.bid = bid
-        self.state.phase = "begin_game"
 
         self.croupier.hand.add(self.decks.get(), face_up=False)
         self.croupier.hand.add(self.decks.get(), face_up=True)
         self.player.hand.add(self.decks.get(), face_up=True)
         self.player.hand.add(self.decks.get(), face_up=True)
 
+        self.state.phase = "begin_game"
+
     @game_action("begin_game", "in_game")
     def get_card(self):
         self.player.hand.add(self.decks.get(), face_up=True)
+
+        self.state.phase = "in_game"
 
     @game_action("begin_game", "in_game")
     def pas(self):
         self.player.hand.playing = False
 
-    @game_action("begin_game")
-    def double_down(self):
-        # TODO
-        pass
+        self.state.phase = "in_game"
 
     @game_action("begin_game")
-    def split(self):  # TODO check this
+    def double_down(self):
+        self.player.account_balance -= self.state.bid
+        self.state.bid *= 2
+
+        self.player.hand.add(self.decks.get(), face_up=True)
+
+        self.state.phase = "in_game"
+
+    @game_action("begin_game")
+    def split(self):
+        # TODO double bid and split into two hands only if rank matches
         if self.did_split:
             raise InvalidMove("Already did split")
 
@@ -127,6 +174,9 @@ class Table:
 
     @game_action("begin_game")
     def insure(self):
+        # TODO put additional 0.5 bid if croupier has ace (blackjack possibility)
+        raise InvalidMove("Not yet implemented")
+
         if self.did_insure:
             raise InvalidMove("Already insured")
         if self.croupier.hand.cards[1].rank != 1:
@@ -135,5 +185,5 @@ class Table:
         # TODO finish
 
     def surrender(self):
-        # TODO does it exist in rules?
+        # TODO abandon bid and start new game (open croupier's card ?)
         pass
