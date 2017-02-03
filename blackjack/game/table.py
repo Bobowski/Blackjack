4f7f37700c2f93b566f2200ba5dee450ce6ec4d2
Adam Bobowski <bobowskiadam@gmail.com>
2017-02-03 16:35:00 +0100

    refactored

--- blackjack/game/table.py
+++ blackjack/game/table.py
@@ -86,6 +86,28 @@ class Croupier:
         self.hand.clear()
 
 
+def action(from_phases, to_phase):
+    def decorator(foo):
+        @wraps(foo)
+        def wrapper(self, *args, **kw):
+            # Do not allow execution of command if not in proper phase
+            if self.state.phase not in from_phases:
+                raise InvalidMove("Not in proper phase")
+
+            # Execute action and switch to target phase
+            foo(self, *args, **kw)
+            self.state.phase = to_phase
+
+            # Switch if other hand is still playing, resolve if none is
+            if not self.player.hand.playing:
+                if self.player.other_hand.playing:
+                    self.player.switch_hand()
+                else:
+                    self.resolve_game()
+        return wrapper
+    return decorator
+
+
 class Table:
     def __init__(self, account_balance: int, seed: int=42):
         self.state = State(phase="awaiting", bid=0, winnings=0)
@@ -97,61 +119,39 @@ class Table:
         self.did_double = False
         self.did_insure = False
 
-    def game_action(*phases):
-        def decorator(f):
-            @wraps(f)
-            def wrapper(self, *args, **kw):
-                if self.state.phase not in phases:
-                    raise InvalidMove("Not in proper phase")
+    def resolve_game(self):
+        self.state.phase = "end_game"
 
-                f(self, *args, **kw)
+        croupier_hand = self.croupier.hand
+        croupier_hand.cards[0].face_up = True
+        croupier_hand.cards[1].face_up = True
 
-                if self.player.hand.value > 21:
-                    self.player.hand.playing = False
+        valid_hands = (x for x in self.player.hands if 0 < x.value <= 21)
+        for player_hand in valid_hands:
+            if croupier_hand.has_blackjack and player_hand.has_blackjack:
+                multiplier = 1
+            elif croupier_hand.has_blackjack:
+                multiplier = 0
+            elif player_hand.has_blackjack:
+                multiplier = 2
+            else:
+                # No blackjack scenario
+                # Croupier has defined strategy
+                while croupier_hand.value <= 16:
+                    croupier_hand.add(self.decks.get())
 
-                if not self.player.hand.playing and self.player.other_hand.playing:
-                    self.player.switch_hand()
-                    return
+                if croupier_hand.value > 21 or player_hand.value > croupier_hand.value:
+                    multiplier = 2
+                elif player_hand.value == croupier_hand.value:
+                    multiplier = 1
+                else:
+                    multiplier = 0
 
-                if not self.player.hand.playing:
-                    self.state.phase = "end_game"
+            self.state.winnings += self.state.bid * multiplier
+        self.player.account_balance += self.state.winnings
+        self.state.phase = "end_game"
 
-                    self.croupier.hand.cards[0].face_up = True
-                    self.croupier.hand.cards[1].face_up = True
-
-                    for hand in self.player.hands:
-                        if hand.value == 0:
-                            continue
-
-                        if hand.value > 21:
-                            continue
-
-                        if hand.has_blackjack:
-                            if self.croupier.hand.has_blackjack:
-                                self.state.winnings += self.state.bid
-                            else:
-                                self.state.winnings += self.state.bid * 2
-                            continue
-
-                        if self.croupier.hand.has_blackjack:
-                            continue
-
-                        while self.croupier.hand.value <= 16:
-                            self.croupier.hand.add(self.decks.get())
-
-                        if self.croupier.hand.value > 21:
-                            self.state.winnings += self.state.bid * 2
-                        elif hand.value == self.croupier.hand.value:
-                            self.state.winnings += self.state.bid
-                        elif hand.value > self.croupier.hand.value:
-                            self.state.winnings += self.state.bid * 2
-
-                    self.player.account_balance += self.state.winnings
-                    self.state.phase = "end_game"
-            return wrapper
-        return decorator
-
-    @game_action("awaiting", "end_game")
+    @action(from_phases=("awaiting", "end_game"), to_phase="begin_game")
     def begin_game(self, bid: int):
         self.croupier.clear()
         self.player.clear()
@@ -166,30 +166,24 @@ class Table:
         self.player.hand.add(self.decks.get(), face_up=True)
         self.player.hand.add(self.decks.get(), face_up=True)
 
-        self.state.phase = "begin_game"
-
-    @game_action("begin_game", "in_game")
+    @action(from_phases=("begin_game", "in_game"), to_phase="in_game")
     def hit(self):
         self.player.hand.add(self.decks.get(), face_up=True)
+        if self.player.hand.value > 21:
+            self.player.hand.playing = False
 
-        self.state.phase = "in_game"
-
-    @game_action("begin_game", "in_game")
+    @action(from_phases=("begin_game", "in_game"), to_phase="in_game")
     def stand(self):
         self.player.hand.playing = False
 
-        self.state.phase = "in_game"
-
-    @game_action("begin_game")
+    @action(from_phases="begin_game", to_phase="in_game")
     def double_down(self):
         self.player.account_balance -= self.state.bid
         self.state.bid *= 2
         self.player.hand.add(self.decks.get(), face_up=True)
         self.player.hand.playing = False
 
-        self.state.phase = "in_game"
-
-    @game_action("begin_game")
+    @action(from_phases=("begin_game",), to_phase="in_game")
     def split(self):
         if self.did_split:
             raise InvalidMove("Already did split")
@@ -205,12 +199,12 @@ class Table:
         else:
             raise InvalidMove("Cannot split cards")
 
-    @game_action("begin_game")
+    @action(from_phases=("begin_game",), to_phase="in_game")
     def insure(self):
         # TODO put additional 0.5 bid if croupier has ace (blackjack possibility)
         raise InvalidMove("Not yet implemented")
 
-    @game_action("in_game", "begin_game")
+    @action(from_phases=("in_game",), to_phase="begin_game")
     def surrender(self):
         # TODO abandon bid and start new game (open croupier's card ?)
         raise InvalidMove("Not yet implemented")
