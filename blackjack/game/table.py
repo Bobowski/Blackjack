5b8dd7ec53e0361d38311f6a31ce226f0194bb56
Adam Bobowski <bobowskiadam@gmail.com>
2017-02-03 16:43:29 +0100

    Splited functionallity into separate modules

--- blackjack/game/table.py
+++ blackjack/game/table.py
@@ -2,88 +2,11 @@ from functools import wraps
 
 from namedlist import namedlist
 
-from blackjack.game.decks import Decks, Card
-
+from blackjack.game.decks import Decks
+from blackjack.game.exceptions import InvalidMove
+from blackjack.game.players import Player, Croupier
 
 State = namedlist("State", ["phase", "bid", "winnings"])
-
-
-class InvalidMove(Exception):
-    def __init__(self, message):
-        Exception.__init__(self)
-        self.message = message
-
-
-class Hand:
-    def __init__(self):
-        self.playing = False
-        self.cards = []
-
-    def add(self, card: Card, face_up: bool=True):
-        self.playing = True
-        card.face_up = face_up
-        self.cards.append(card)
-
-    def clear(self):
-        self.cards.clear()
-
-    @property
-    def value(self) -> int:
-        cards = [x for x in self.cards if x.face_up]
-        aces = len([x for x in cards if x.rank == 1])
-        value = sum([10 if x.rank > 10 else x.rank for x in cards]) + aces * 10
-
-        if value <= 21 or aces == 0:
-            return value
-
-        while aces > 0 and value > 21:
-            value -= 10
-            aces -= 1
-        return value
-
-    @property
-    def has_blackjack(self) -> bool:
-        return self.value == 21 and len(self.cards) == 2
-
-
-class Player:
-    def __init__(self, account_balance: int):
-        self._balance = account_balance
-        self.hands = (Hand(), Hand())
-
-    @property
-    def account_balance(self) -> int:
-        return self._balance
-
-    @account_balance.setter
-    def account_balance(self, value: int):
-        if value < 0:
-            raise InvalidMove("Negative account balance is not allowed")
-        self._balance = value
-
-    def switch_hand(self):
-        if self.hands[1].playing:
-            self.hands = (self.hands[1], self.hands[0])
-
-    @property
-    def hand(self) -> Hand:
-        return self.hands[0]
-
-    @property
-    def other_hand(self):
-        return self.hands[1]
-
-    def clear(self):
-        for hand in self.hands:
-            hand.clear()
-
-
-class Croupier:
-    def __init__(self):
-        self.hand = Hand()
-
-    def clear(self):
-        self.hand.clear()
 
 
 def action(from_phases, to_phase):
@@ -176,7 +99,7 @@ class Table:
     def stand(self):
         self.player.hand.playing = False
 
-    @action(from_phases="begin_game", to_phase="in_game")
+    @action(from_phases=("begin_game",),  to_phase="in_game")
     def double_down(self):
         self.player.account_balance -= self.state.bid
         self.state.bid *= 2
